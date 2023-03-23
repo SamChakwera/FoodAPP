a76475af3efb22b79098fd7920dc8beb94cd3770
SamChakwera <47982207+SamChakwera@users.noreply.github.com>
2023-03-23 00:51:58 -0600

    Update Fride2DishApp.py

--- Fride2DishApp.py
+++ Fride2DishApp.py
@@ -56,7 +56,7 @@ def generate_images(dishes):
 
     model_id = "runwayml/stable-diffusion-v1-5"
     pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
-    pipe = pipe.to("cpu")
+    pipe = pipe.to("cuda")
 
     images = []
     for dish in truncated_dishes:
